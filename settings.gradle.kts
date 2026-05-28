a24cb3e21ec69869fe50649e997cb83e102e448d
St4ckC00ki3 <60530949+StackC00ki3@users.noreply.github.com>
2026-05-28 13:05:53 +0800

    fix maven mirror

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,27 +1,25 @@
 pluginManagement {
     repositories {
-        maven { url=uri("https://maven.aliyun.com/repository/public")}
-        maven { url=uri("https://maven.aliyun.com/repository/google")}
-        maven { url=uri("https://maven.aliyun.com/repository/central")}
-        maven { url=uri("https://maven.aliyun.com/repository/gradle-plugin")}
-        maven { url=uri("https://plugins.gradle.org/m2/")}
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { url=uri("https://www.jitpack.io")}
-        maven { url=uri("https://maven.aliyun.com/repository/releases")}
-        maven { url=uri("https://maven.aliyun.com/repository/google")}
-        maven { url=uri("https://maven.aliyun.com/repository/central")}
-        maven { url=uri("https://maven.aliyun.com/repository/gradle-plugin")}
-        maven { url=uri("https://maven.aliyun.com/repository/public")}
-        maven { url=uri("https://plugins.gradle.org/m2/")}
         google()
         mavenCentral()
+        gradlePluginPortal()
+        maven { url = uri("https://www.jitpack.io") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
     }
 }
 
